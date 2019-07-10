d76a89aefea4d3399cb163a0c899fa4c1b0824a0
EugeneReznik16051986 <50756243+EugeneReznik16051986@users.noreply.github.com>
2019-07-10 20:06:59 +0300

    javascript

---
+++ Lesson17/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/*1*/
+const A = 60;
+const B = 60;
+const C = 3;
+const D = A * B * C;
+console.log(D);
+
+/*2*/
+var num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num ++;
+num --;
+alert(num);
+
+/*3*/
+var a = 10; 
+var b = 2;
+var c = 5;
+var d = a + b +c;
+console.log(d);
+
+/*4*/
+var a = 17; 
+var b = 10;
+var c = a - b;
+var d = 7;
+var result = c + d;
+console.log( result );
+
+/*5*/
+var name = 'Евгений';
+console.log('Привет,' + ' ' + name + '!');
